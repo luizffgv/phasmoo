962662349253f9b65a5cbbdc7e4cbf402320bd45
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-03-15 19:35:54 -0300

    Card can now receive additional classes

--- src/app/_components/card.tsx
+++ src/app/_components/card.tsx
@@ -1,6 +1,14 @@
-export default function Card({ children }: { children?: React.ReactNode }) {
+export default function Card({
+  children,
+  className = "",
+}: {
+  children?: React.ReactNode;
+  className?: string;
+}) {
   return (
-    <div className="w-fit max-w-full rounded-2xl bg-slate-50 p-4 shadow-md transition-colors dark:bg-slate-700">
+    <div
+      className={`w-fit max-w-full rounded-2xl bg-slate-50 p-4 shadow-md transition-colors dark:bg-slate-700 ${className}`}
+    >
       {children}
     </div>
   );
