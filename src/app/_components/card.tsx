5b8da4b885a7310babe9ed21162fa64cb01c91a8
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-03-15 20:32:41 -0300

    Fix Ghost flex-grow without limits

--- src/app/_components/card.tsx
+++ src/app/_components/card.tsx
@@ -7,7 +7,7 @@ export default function Card({
 }) {
   return (
     <div
-      className={`w-fit max-w-full rounded-2xl bg-stone-50 p-4 shadow-md transition-colors dark:bg-stone-800 ${className}`}
+      className={`rounded-2xl bg-stone-50 p-4 shadow-md transition-colors dark:bg-stone-800 ${className}`}
     >
       {children}
     </div>
--- src/app/_components/evidence-picker.tsx
+++ src/app/_components/evidence-picker.tsx
@@ -4,7 +4,7 @@ import Card from "./card";
 
 export default function EvidencePicker() {
   return (
-    <Card>
+    <Card className="max-w-full">
       <div className="flex w-full flex-row gap-x-4 gap-y-2 overflow-x-auto rounded-xl sm:max-w-[512px] sm:flex-wrap">
         {EvidenceID.map((id) => (
           <EvidenceButton key={id} evidenceID={id}></EvidenceButton>
