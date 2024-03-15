ba7331fa71633fcd4d1cc71c9b6d06da229eedae
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-03-15 18:06:20 -0300

    Add fade-in animation to ghost list

--- src/app/_components/ghost-list.tsx
+++ src/app/_components/ghost-list.tsx
@@ -2,16 +2,30 @@
 
 import { EvidenceContext } from "@/contexts/evidence";
 import { GHOSTS, filter } from "@/lib/phasmo";
-import { useContext, useMemo } from "react";
+import { useContext, useLayoutEffect, useMemo, useRef } from "react";
 import Ghost from "./ghost";
 
 export default function GhostList() {
   const { evidences } = useContext(EvidenceContext);
+  const listRef = useRef<HTMLDivElement>(null);
 
   const ghostFilter = useMemo(() => filter(evidences.value, 3), [evidences]);
 
+  useLayoutEffect(() => {
+    if (listRef.current == null) {
+      console.error("listRef.current is null");
+      return;
+    }
+
+    new Animation(
+      new KeyframeEffect(listRef.current, [{ opacity: 0 }, {}], {
+        duration: 250,
+      })
+    ).play();
+  }, [evidences]);
+
   return (
-    <div className="flex flex-column flex-wrap gap-4">
+    <div ref={listRef} className="flex flex-column flex-wrap gap-4">
       {GHOSTS.filter(ghostFilter).map((ghost) => (
         <Ghost key={ghost.name} ghost={ghost}></Ghost>
       ))}
