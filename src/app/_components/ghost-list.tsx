bb9778e56acb1a5e232dd6cb44ca3eac7d5d82e5
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-03-15 20:04:13 -0300

    Make some containers rounded

--- src/app/_components/ghost-list.tsx
+++ src/app/_components/ghost-list.tsx
@@ -30,7 +30,7 @@ export default function GhostList() {
   return (
     <div
       ref={listRef}
-      className="flex-column flex flex-wrap justify-center gap-4 overflow-y-auto"
+      className="flex-column flex flex-wrap justify-center gap-4 overflow-y-auto rounded-2xl "
     >
       {ghosts.map((ghost) => (
         <Ghost key={ghost.name} ghost={ghost}></Ghost>
