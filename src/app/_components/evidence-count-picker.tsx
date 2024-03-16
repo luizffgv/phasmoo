3d0835f9bc1bd996467a52416a10b4ead09f0511
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-03-16 15:25:48 -0300

    EvidenceCountPicker now waits for settings if needed

--- src/app/_components/evidence-count-picker.tsx
+++ src/app/_components/evidence-count-picker.tsx
@@ -1,31 +1,47 @@
 "use client";
 
 import { EvidenceCountContext } from "@/contexts/evidence-count";
-import { useContext } from "react";
+import { useContext, useLayoutEffect, useRef } from "react";
 import Card from "./card";
 import RadioButton from "./radio-button";
 
 export default function EvidenceCountPicker() {
   const { count, setCount } = useContext(EvidenceCountContext);
 
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    new Animation(
+      new KeyframeEffect(containerRef.current, [{}, { opacity: 1 }], {
+        duration: 250,
+        fill: "forwards",
+      }),
+    ).play();
+  }, []);
+
   return (
-    <Card className="w-fit">
-      <div className="flex flex-row flex-wrap items-baseline justify-center gap-x-8 gap-y-2">
-        <strong>Available evidence</strong>
-        <div className="flex flex-row flex-wrap gap-2" role="radiogroup">
-          {([1, 2, 3] as const).map((num) => (
-            <RadioButton
-              key={num}
-              onClick={() => setCount(num)}
-              checked={num == count}
-              aria-label={`${num} evidence`}
-              aria-labelledby="y"
-            >
-              {num}
-            </RadioButton>
-          ))}
+    // We use opacity-0 because in smaller screens the picker's position is
+    // affected by the settings, so we wait until settings are applied before
+    // showing it.
+    <div ref={containerRef} className="opacity-0 sm:opacity-100">
+      <Card className="w-fit">
+        <div className="flex flex-row flex-wrap items-baseline justify-center gap-x-8 gap-y-2">
+          <strong>Available evidence</strong>
+          <div className="flex flex-row flex-wrap gap-2" role="radiogroup">
+            {([1, 2, 3] as const).map((num) => (
+              <RadioButton
+                key={num}
+                onClick={() => setCount(num)}
+                checked={num == count}
+                aria-label={`${num} evidence`}
+                aria-labelledby="y"
+              >
+                {num}
+              </RadioButton>
+            ))}
+          </div>
         </div>
-      </div>
-    </Card>
+      </Card>
+    </div>
   );
 }
