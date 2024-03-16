f075c67dc41d3ad148c9f7ce8d9a9ad58312553e
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-03-16 16:51:40 -0300

    Change Button centering classes

--- src/app/_components/button.tsx
+++ src/app/_components/button.tsx
@@ -64,7 +64,7 @@ export default function Button(props: Props) {
         <Link
           href={props.href}
           target={props.target}
-          className={`rounded-xl px-4 py-2 text-center font-bold transition-all ${text} ${bg} ${hover} ${props.className ?? ""}`}
+          className={`flex flex-row justify-center rounded-xl px-4 py-2 font-bold transition-all ${text} ${bg} ${hover} ${props.className ?? ""}`}
           aria-label={props["aria-label"]}
           aria-labelledby={props["aria-labelledby"]}
         >
@@ -72,7 +72,7 @@ export default function Button(props: Props) {
         </Link>
       ) : (
         <button
-          className={`rounded-xl px-4 py-2 font-bold transition-all disabled:opacity-50 ${text} ${bg} ${hover} ${props.className ?? ""}`}
+          className={`flex flex-row justify-center rounded-xl px-4 py-2 font-bold transition-all disabled:opacity-50 ${text} ${bg} ${hover} ${props.className ?? ""}`}
           role={props["role"]}
           aria-label={props["aria-label"]}
           aria-checked={props["aria-checked"]}
