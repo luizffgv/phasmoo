548992768f6c5ef74953c7f34e6c26b0640ce782
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-03-19 01:27:31 -0300

    Clicking the logo now brings you to home

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,13 +1,16 @@
+import Link from "next/link";
 import Logo from "./logo";
 import Settings from "./settings";
 
 export default function Header() {
   return (
     <header className="flex flex-row justify-between">
-      <div className="flex flex-row items-center gap-4">
-        <Logo fill="text-stone-700 dark:text-stone-100"></Logo>
-        <h1 className="text-lg font-bold">Phasmoo</h1>
-      </div>
+      <Link href="/">
+        <div className="flex flex-row items-center gap-4">
+          <Logo fill="text-stone-700 dark:text-stone-100"></Logo>
+          <h1 className="text-lg font-bold">Phasmoo</h1>
+        </div>
+      </Link>
       <Settings></Settings>
     </header>
   );
